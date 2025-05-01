0dc835f4befe176ff890bb9a3985ca697a219fc9
IvanYingX <ying.yingxuan@hotmail.com>
2025-05-01 17:08:53 +0200

    Adds custom errors

---
+++ backend/app/models/errors.py
@@ -0,0 +1,7 @@
+class AIServiceError(Exception):
+    """Custom exception for AI service-related errors"""
+    pass
+
+class DocumentProcessingError(Exception):
+    """Custom exception for document processing errors"""
+    pass
